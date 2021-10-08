50d72b8c706d4bd048ea00fe87a9710a6bc154c1
Kevin Zhang <hongtao_zhang@outlook.com>
2021-10-07 21:20:04 -0500

    Update dependency and add language check

--- Flow.LibreTranslate/Main.cs
+++ Flow.LibreTranslate/Main.cs
@@ -1,88 +1,112 @@
+using Flow.Launcher.Plugin;
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using Flow.Launcher.Plugin;
+using System.Linq;
+using System.Net.Http;
 using System.Text.Json;
 using System.Threading;
-using System.Net.Http;
-using System.Linq;
+using System.Threading.Tasks;
 
-namespace Flow.Launcher.Plugin.LibreTranslate
+namespace Flow.LibreTranslate
 {
     public class LibreTranslate : IAsyncPlugin
     {
         private PluginInitContext _context;
-        internal List<Language> supportedLanguage;
+        internal List<Language> SupportedLanguage;
 
-        internal const string LanguageListUrl = "https://libretranslate.com/languages";
-        internal const string TranslatetUrl = "https://libretranslate.com/translate";
+        internal const string LibreTranslateUrl = "https://translate.argosopentech.com";
+        internal const string LanguageListUrl = LibreTranslateUrl + "/languages";
+        internal const string TranslateUrl = LibreTranslateUrl + "/translate";
 
         internal const string IconPath = "Images\\translate.png";
 
         public async Task InitAsync(PluginInitContext context)
         {
-            _context = context;
+            this._context = context;
 
-            using var stream = await _context.API.HttpGetStreamAsync(LanguageListUrl);
+            await using var stream = await this._context.API.HttpGetStreamAsync(LanguageListUrl);
 
-            supportedLanguage = await JsonSerializer.DeserializeAsync<List<Language>>(stream);
+            SupportedLanguage = await JsonSerializer.DeserializeAsync<List<Language>>(stream);
         }
 
-        private HttpClient client = new HttpClient();
+        private readonly HttpClient _client = new();
 
         public async Task<List<Result>> QueryAsync(Query query, CancellationToken token)
         {
-            string[] searchs = query.Search.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var searches = query.Search.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-            bool semiQueryCompleted = searchs.Length == 0 || supportedLanguage.Any(x => x.Code == searchs[^1]);
+            var semiQueryCompleted = searches.Length == 0 || SupportedLanguage.Any(x => x.Code == searches[^1]);
 
-            if (searchs.Length < 3)
+            if (searches.Length < 3)
             {
-                if (searchs.Length == 2 && semiQueryCompleted)
+                if (searches.Length == 2 && semiQueryCompleted)
                     return new List<Result>();
-                return supportedLanguage.Select(l => new Result
-                {
-                    Title = l.Name,
-                    SubTitle = l.Code,
-                    Action = _ =>
+                return SupportedLanguage.Select(l => new Result
                     {
-                        _context.API.ChangeQuery(
-                            $"{_context.CurrentPluginMetadata.ActionKeyword} {(semiQueryCompleted ? query.Search : string.Join(' ', searchs[..^1]))}{(searchs.Length == 0 ? "" : " ")}{l.Code} "
-                        );
-                        return false;
-                    },
-                    IcoPath = IconPath,
-                    Score = semiQueryCompleted ? 100 : _context.API.FuzzySearch(searchs[^1], l.Name).Score
-                }).Where(r => r.Score > 0)
-                .ToList();
+                        Title = l.Name,
+                        SubTitle = l.Code,
+                        Action = _ =>
+                        {
+                            _context.API.ChangeQuery(
+                                $"{_context.CurrentPluginMetadata.ActionKeyword} {(semiQueryCompleted ? query.Search : string.Join(' ', searches[..^1]))}{(searches.Length == 0 ? "" : " ")}{l.Code} "
+                            );
+                            return false;
+                        },
+                        IcoPath = IconPath,
+                        Score = semiQueryCompleted ? 100 : _context.API.FuzzySearch(searches[^1], l.Name).Score
+                    }).Where(r => r.Score > 0)
+                    .ToList();
             }
 
-            var from = supportedLanguage.Find(x => x.Code == searchs[0]);
-            var to = supportedLanguage.Find(x => x.Code == searchs[1]);
+            var from = SupportedLanguage.Find(x => x.Code == searches[0]);
+            var to = SupportedLanguage.Find(x => x.Code == searches[1]);
 
-            string text = string.Join(' ', searchs[2..]);
+            if (from is null || to is null)
+            {
+                return new List<Result>
+                {
+                    new()
+                    {
+                        Title = "One of the Language code does not exist", IcoPath = IconPath
+                    }
+                };
+            }
+
+            string text = string.Join(' ', searches[2..]);
             var requestContent = new Dictionary<string, string>
             {
-                {"q",text},
-                {"source", from.Code},
-                {"target", to.Code},
-                {"api_key",""}
+                {
+                    "q", text
+                },
+                {
+                    "source", from.Code
+                },
+                {
+                    "target", to.Code
+                },
+                {
+                    "api_key", ""
+                }
             };
 
-            await Task.Delay(150);
+
+            await Task.Delay(150, token);
             token.ThrowIfCancellationRequested();
 
-            FormUrlEncodedContent content = new FormUrlEncodedContent(requestContent);
-            using var response = await client.PostAsync(TranslatetUrl, content, token);
+            var content = new FormUrlEncodedContent(requestContent);
+            using var response = await _client.PostAsync(TranslateUrl, content, token);
 
-            var result = await JsonDocument.ParseAsync(await response.Content.ReadAsStreamAsync());
+            await using var resultStream = await response.Content.ReadAsStreamAsync(token);
+            using var result = await JsonDocument.ParseAsync(resultStream, cancellationToken: token);
 
-            return new List<Result>{
-                new Result{
-                    Title=$"Translated Text: {result.RootElement.GetProperty("translatedText")}",
-                    SubTitle=$"{text}: From {from.Name} to {to.Name}",
-                    Score=100,
-                    IcoPath=IconPath
+            return new List<Result>
+            {
+                new()
+                {
+                    Title = $"Translated Text: {result.RootElement.GetProperty("translatedText")}",
+                    SubTitle = $"{text}: From {from.Name} to {to.Name}",
+                    Score = 100,
+                    IcoPath = IconPath
                 }
             };
 
